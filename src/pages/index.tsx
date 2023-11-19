a73bc8b3f1987e975a86b41f374c019149aa5ee6
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-11-18 21:39:13 -0400

    instalaçao das bibliotecas e ligação com o supabase

---
+++ src/pages/index.tsx
@@ -0,0 +1,138 @@
+import Image from "next/image";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { AlbumArtwork } from "@/components/album-artwork";
+import { Menu } from "@/components/menu";
+import { PodcastEmptyPlaceholder } from "@/components/podcast-empty-placeholder";
+import { Sidebar } from "@/components/sidebar";
+import { madeForYouAlbums } from "@/data/albums";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "@/utils/api";
+
+import Layout from "./layout";
+
+export default function Home() {
+
+/* const {isLoading, data} = await api. */
+
+  return (
+    <Layout>
+      <div className="h-full px-4 py-6 lg:px-8">
+        <Tabs defaultValue="music" className="h-full space-y-6">
+          <div className="space-between flex items-center">
+            <TabsList>
+              <TabsTrigger value="music" className="relative">
+                Music
+              </TabsTrigger>
+              <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
+              <TabsTrigger value="live" disabled>
+                Live
+              </TabsTrigger>
+            </TabsList>
+            <div className="ml-auto mr-4">
+              <Button>
+                <PlusCircledIcon className="mr-2 h-4 w-4" />
+                Add music
+              </Button>
+            </div>
+          </div>
+          <TabsContent value="music" className="border-none p-0 outline-none">
+            <div className="flex items-center justify-between">
+              <div className="space-y-1">
+                <h2 className="text-2xl font-semibold tracking-tight">
+                  Listen Now
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                  Top picks for you. Updated daily.
+                </p>
+              </div>
+            </div>
+            <Separator className="my-4" />
+            <div className="relative">
+              <ScrollArea>
+                <div className="flex space-x-4 pb-4"></div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+            </div>
+            <div className="mt-6 space-y-1">
+              <h2 className="text-2xl font-semibold tracking-tight">
+                Made for You
+              </h2>
+              <p className="text-sm text-muted-foreground">
+                Your personal playlists. Updated daily.
+              </p>
+            </div>
+            <Separator className="my-4" />
+            <div className="relative">
+              <ScrollArea>
+                <div className="flex space-x-4 pb-4">
+                  {madeForYouAlbums.map((album) => (
+                    <AlbumArtwork
+                      key={album.name}
+                      album={album}
+                      className="w-[150px]"
+                      aspectRatio="square"
+                      width={150}
+                      height={150}
+                    />
+                  ))}
+                </div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+            </div>
+          </TabsContent>
+          <TabsContent
+            value="podcasts"
+            className="h-full flex-col border-none p-0 data-[state=active]:flex"
+          >
+            <div className="flex items-center justify-between">
+              <div className="space-y-1">
+                <h2 className="text-2xl font-semibold tracking-tight">
+                  New Episodes
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                  Your favorite podcasts. Updated daily.
+                </p>
+              </div>
+            </div>
+            <Separator className="my-4" />
+            <PodcastEmptyPlaceholder />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </Layout>
+  );
+}
+
+/* function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}*/
